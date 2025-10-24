5609ab11f0342b6fd2048ccd63f26e586175a040
Anis-Dangol <anis.dangol121@gmail.com>
2025-10-24 15:58:32 +0545

    initial commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import express, { Router } from "express";
+
+const router = express.Router();
+
+router.get("/send", (req, res) => {
+  res.send("Send mesage endpoint");
+});
+
+export default router;
