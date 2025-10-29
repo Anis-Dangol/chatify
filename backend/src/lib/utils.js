255dc2ae3f8f00b039cc5ae6becb167e7a2a3e6b
Anis-Dangol <anis.dangol121@gmail.com>
2025-10-29 12:16:56 +0545

    login and logout functionality added

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,7 +2,12 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign(ENV.JWT_SECRET, {
+  const { JWT_SECRET } = ENV;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
+
+  const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
 
